a52c5a2dd879f918f56c853cdfefe00302d38e51
Alvaro Gamez Chavez <gaositd@gmail.com>
2023-08-17 22:36:20 -0600

    mas de 30 capitulos del curso de java, son 229 jejejeeje

--- src/main/java/com/cursojava/cursojavaspringboot/javaPuro/clase30/poo/Clase30.java
+++ src/main/java/com/cursojava/cursojavaspringboot/javaPuro/clase30/poo/Clase30.java
@@ -1,13 +1,16 @@
 package com.cursojava.cursojavaspringboot.javaPuro.clase30.poo;
 
 public class Clase30 {
-//coche https://www.youtube.com/watch?v=ZY5pwm92cWQ&list=PLU8oAlHdN5BktAXdEVCLUYzvDyqRQJ2lk&index=31
+//coche https://www.youtube.com/watch?v=ZY5pwm92cWQ&list=PLU8oAlHdN5BktAXdEVCLUYzvDyqRQJ2lk&index=34
     private String marcaModelo;
     private int ruedas;
     private int largo;
     private int ancho;
     private int motor;
-    private int peso;
+    private int pesoPlataforma;
+    private String color;
+    private int pesoTotal;
+    private boolean asientosCuero;
 
     public Clase30(){
         marcaModelo ="Renult alfaRomeo 2023";
@@ -15,24 +18,39 @@ public class Clase30 {
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        pesoPlataforma = 500;
     }
 
-    public Clase30(String MarcaModelo, int Ruedas, int Largo, int Ancho, int Motor, int Peso){
+    public Clase30(
+            String MarcaModelo,
+            int Ruedas,
+            int Largo,
+            int Ancho,
+            int Motor,
+            int PesoPlataforma,
+            String Color,
+            int PesoTotal,
+            boolean AsientosCuero
+    ){
         this.marcaModelo = MarcaModelo;
         this.ruedas = Ruedas;
         this.largo = Largo;
         this.ancho = Ancho;
         this.motor = Motor;
-        this.peso = Peso;
+        this.pesoPlataforma = PesoPlataforma;
+        this.color = Color;
+        this.pesoTotal = PesoTotal;
+        this.asientosCuero = AsientosCuero;
     }
 
     public String getMarcaModelo() {
-        return MarcaModelo;
+        
+        return this.marcaModelo;
     }
 
-    public void setMarcaModelo(String marcaModelo) {
-        MarcaModelo = marcaModelo;
+    public void setMarcaModelo(String MarcaModelo) {
+        
+        this.marcaModelo = MarcaModelo;
     }
 
     public int getRuedas() {
@@ -67,12 +85,40 @@ public class Clase30 {
         this.motor = motor;
     }
 
-    public int getPeso() {
-        return peso;
+    public int getPesoPlataforma() {
+        return this.pesoPlataforma;
     }
 
-    public void setPeso(int peso) {
-        this.peso = peso;
+    public void setpesoPlataforma(int PesoPlataforma) {
+        this.pesoPlataforma = PesoPlataforma;
+    }
+
+    public void setPesoPlataforma(int pesoPlataforma) {
+        this.pesoPlataforma = pesoPlataforma;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPesoTotal() {
+        return pesoTotal;
+    }
+
+    public void setPesoTotal(int pesoTotal) {
+        this.pesoTotal = pesoTotal;
+    }
+
+    public boolean isAsientosCuero() {
+        return asientosCuero;
+    }
+
+    public void setAsientosCuero(boolean asientosCuero) {
+        this.asientosCuero = asientosCuero;
     }
 
     @Override
@@ -82,7 +128,7 @@ public class Clase30 {
                 ", largo=" + largo +
                 ", ancho=" + ancho +
                 ", motor=" + motor +
-                ", peso=" + peso +
+                ", pesoPlataforma=" + pesoPlataforma +
                 '}';
     }
 }
--- src/main/java/com/cursojava/cursojavaspringboot/javaPuro/clase30/poo/UsoCoche.java
+++ src/main/java/com/cursojava/cursojavaspringboot/javaPuro/clase30/poo/UsoCoche.java
@@ -5,17 +5,20 @@ public class UsoCoche {
 
     public static void main(String[] args) {
         Clase30 carro1 = new Clase30();
-        Clase30 carro2 = new Clase30("Nissan Sentra 2017",6,5000,3000,3000,2000);
+        Clase30 carro2 = new Clase30("Nissan Sentra 2017",6,5000,3000,3000,2000,"Rojo",4245,true);
 
         carro1.setMarcaModelo("Chevrolet S10 2023");
         carro1.setRuedas(4);
         carro1.setLargo(3000);
         carro1.setAncho(1000);
         carro1.setMotor(1600);
-        carro1.setPeso(800);
+        carro1.setpesoPlataforma(800);
+        carro1.setColor("Azul");
+        carro1.setPesoTotal(3456);
+        carro1.setAsientosCuero(false);
 
-        System.out.println("Datos del carro 1 =  Marca y Modelo = " + carro1.getMarcaModelo()+" Ruedas = "+carro1.getRuedas()+" largo = "+carro1.getLargo()+" Ancho = "+carro1.getAncho()+" Motor = "+carro1.getMotor()+" Peso = "+carro1.getPeso());
+        System.out.println("Datos del carro 1 =  Marca y Modelo = " + carro1.getMarcaModelo()+" Ruedas = "+carro1.getRuedas()+" largo = "+carro1.getLargo()+"cm Ancho = "+carro1.getAncho()+"cm Motor = "+carro1.getMotor()+"cm3 Peso = "+carro1.getPesoPlataforma() +"kg "+carro1.getColor() +" Peso total "+ carro1.getPesoTotal()+ "kg ¿Asientos de cuero?" + (carro1.isAsientosCuero() ? " Si" : "No"));
         System.out.println();
-        System.out.println("Datos del carro 2 =  Marca y Modelo = " + carro2.getMarcaModelo()+" Ruedas = "+carro2.getRuedas()+" largo = "+carro2.getLargo()+" Ancho = "+carro2.getAncho()+" Motor = "+carro2.getMotor()+" Peso = "+carro2.getPeso());
+        System.out.println("Datos del carro 2 =  Marca y Modelo = " + carro2.getMarcaModelo()+" Ruedas = "+carro2.getRuedas()+" largo = "+carro2.getLargo()+"cm Ancho = "+carro2.getAncho()+"cm Motor = "+carro2.getMotor()+"cm3 Peso = "+carro2.getPesoPlataforma() +"kg "+carro2.getColor() +" Peso total "+ carro2.getPesoTotal()+ "Kg ¿Asientos de cuero?" + (carro2.isAsientosCuero() ? " Si" : "No"));
     }
 }
